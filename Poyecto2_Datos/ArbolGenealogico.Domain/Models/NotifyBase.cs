e4925dab7891009d7ce3156efafc3b92a8e78c10
Marcelo-Matey-R <queridozapato@gmail.com>
2025-11-23 19:32:19 -0600

    Update NotifyBase.cs

--- Poyecto2_Datos/ArbolGenealogico.Domain/Models/NotifyBase.cs
+++ Poyecto2_Datos/ArbolGenealogico.Domain/Models/NotifyBase.cs
@@ -6,7 +6,8 @@ namespace ArbolGenealogico.Domain.Models
 {
     public abstract class NotifyBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged; // Evento para notificar cambios en las propiedades
+        // Método auxiliar para establecer el valor de una propiedad y notificar el cambio
         protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
         {
             if (EqualityComparer<T>.Default.Equals(field, value)) return false;
@@ -14,6 +15,7 @@ namespace ArbolGenealogico.Domain.Models
             OnPropertyChanged(propertyName);
             return true;
         }
+        // Método para notificar que una propiedad ha cambiado
         protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             var handler = PropertyChanged;
