7b4e9237af46bcc60645d7e965a4fd0fae8ef7eb
Marcelo-Matey-R <queridozapato@gmail.com>
2025-11-23 19:29:00 -0600

    Update Edge.cs

--- Poyecto2_Datos/ArbolGenealogico.Domain/Models/Edge.cs
+++ Poyecto2_Datos/ArbolGenealogico.Domain/Models/Edge.cs
@@ -5,11 +5,14 @@ namespace ArbolGenealogico.Domain.Models
 {
     public class Edge
     {
+        #region Atributos
         public Node fam1 { get; }
         public Node fam2 { get; }
 
         public double weight { get; }
+        #endregion
 
+        #region Constructor
         public Edge(Node per1, Node per2, double w)
         {
             this.fam1 = per1 ?? throw new ArgumentNullException(nameof(per1));
@@ -17,8 +20,11 @@ namespace ArbolGenealogico.Domain.Models
 
             this.weight = w;
         }
-        public bool IsValid() => !double.IsNaN(weight) && !double.IsInfinity(weight);
-        public override string ToString() => $"{fam1.familiar.name} -> {fam2.familiar.name}: {weight:F6}"; 
+        #endregion
 
+        #region Helpers
+        public bool IsValid() => !double.IsNaN(weight) && !double.IsInfinity(weight);
+        public override string ToString() => $"{fam1.familiar.name} -> {fam2.familiar.name}: {weight:F6}";
+        #endregion
     }
 }
